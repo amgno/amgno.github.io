df35223f02543b550f818e74dfcf1936d33a3bd9
amgno <ale@magno.me>
2024-09-20 17:44:32 +0200

    test

--- server.js
+++ server.js
@@ -113,3 +113,8 @@ app.listen(port, '0.0.0.0', () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
